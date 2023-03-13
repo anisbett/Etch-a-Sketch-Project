b99592408aa3330090b5a2a6c6cefda2393d31e5
anisbett <ajaninisbett@outlook.com>
2023-03-13 17:50:03 -0400

    Done for now

--- external.js
+++ external.js
@@ -55,27 +55,7 @@ document.querySelector("body").addEventListener("click", (e) => {
     if (click) {
         document.querySelector(".mode").textContent = "Mode: Van Gogh";
     } else {
-        document.querySelector(".mode").textContent = "Mode: Not Van Gogh";
+        document.querySelector(".mode").textContent = "Mode: Van No";
     }
     }
-})
-/* function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-    cell.style.width = (100/cols) + '%';
-    cell.style.height = (100/rows) + '%';
-  };
-};
-
-function setGridSize(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  container.innerHTML = "";
-  makeRows(rows, cols);
-}
-
-makeRows(16, 16); */
+});
