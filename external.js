d9792b355120e0eb8afb0041c50d25154be2bf6f
anisbett <ajaninisbett@outlook.com>
2023-03-13 15:17:41 -0400

    Grid and buttons are working

--- external.js
+++ external.js
@@ -1,6 +1,51 @@
-const container = document.getElementById("container");
+let color = "black";
 
-function makeRows(rows, cols) {
+function fillBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+
+    for (let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+} 
+}
+
+fillBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 || input <= 100) {
+        fillBoard(input);
+    }
+    else{
+        console.log("Board size not in range");
+    }
+}
+
+function colorSquare() {
+    if ((color==="random")) {
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = color;
+    }
+} 
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+/* function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
@@ -19,4 +64,4 @@ function setGridSize(rows, cols) {
   makeRows(rows, cols);
 }
 
-makeRows(16, 16);
+makeRows(16, 16); */
